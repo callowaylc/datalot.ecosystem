9c6e9556eeab6e976bc84d9073580f06bcae8db5
Ubuntu <ubuntu@ip-10-203-81-150.ec2.internal>
2013-10-11 21:54:21 +0000

    added proper error handling

--- main.rb
+++ main.rb
@@ -7,8 +7,8 @@
 ### REQUIRES
 
 require 'yaml'
-require 'Species'
-require 'Habitat'
+require_relative 'Species'
+require_relative 'Habitat'
 
 ### CONSTANTS 
 
@@ -18,3 +18,21 @@ FILE_CONFIG = './Data.yaml'
 ### MAIN
 
 # parse yaml file and bind to config variable
+
+parsed = begin
+  YAML.load file = open(FILE_CONFIG)
+
+rescue ArgumentError => e
+  puts "Could not parse YAML: #{e.message}"
+  raise
+
+rescue Exception => e
+  puts "Failed to find configuration file '#{FILE_CONFIG}'"
+  raise
+
+ensure
+    file.close unless file.nil?
+
+end
+
+puts parsed.class
